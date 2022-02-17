48f6d8c40629643406ca2311019f16cc7613af1f
Somsubhra Das <das.somsubhra1@gmail.com>
2022-02-17 18:34:44 +0530

    added comments

--- 3.js
+++ 3.js
@@ -10,7 +10,7 @@ const web3 = new Web3(new Web3(url));
 const account1 = "0x98575121203CA00725676C12d8D862bEC950f2fB";
 const account2 = "0x3295B99072Ec49018386a68ec45dC090224384c3";
 
-const privateKey1 = Buffer.from(process.env.PRIVATE_KEY1, "hex");
+const privateKey1 = Buffer.from(process.env.PRIVATE_KEY1, "hex"); // converting private key to hex format
 
 web3.eth.getBalance(account1, (err, balance) => {
   console.log("-----------GET BALANCE-------------");
@@ -24,7 +24,7 @@ web3.eth.getBalance(account2, (err, balance) => {
   console.log("account2 Balance", web3.utils.fromWei(balance, "ether")); // convert to subsequent units
 });
 
-// send transaction
+// send transaction unsigned
 // web3.eth.sendTransaction(
 //   {
 //     from: account1,
@@ -40,16 +40,18 @@ web3.eth.getBalance(account2, (err, balance) => {
 web3.eth.getTransactionCount(account1, (err, txCount) => {
   // build the transaction
   const txObject = {
-    nonce: web3.utils.toHex(txCount),
-    to: account2,
-    value: web3.utils.toHex(web3.utils.toWei("0.1", "ether")),
-    gasLimit: web3.utils.toHex(21000),
-    gasPrice: web3.utils.toHex(web3.utils.toWei("1", "gwei")),
+    nonce: web3.utils.toHex(txCount), // previous transaction count
+    to: account2, // receiving account
+    value: web3.utils.toHex(web3.utils.toWei("0.1", "ether")), // amount to transfer
+    gasLimit: web3.utils.toHex(21000), // this is the maximum amount of gas consumed by the transaction.
+    gasPrice: web3.utils.toHex(web3.utils.toWei("1", "gwei")), // this is the amount we want to pay for each unit of gas.
+
+    // gas is basically the price we pay to the miners for the transaction to take place.
   };
 
   // sign the txn
-  const tx = new Tx(txObject, { chain: "ropsten" });
-  //   return;
+  const tx = new Tx(txObject, { chain: "ropsten" }); // specify the blockchain here. in production use mainnet
+
   tx.sign(privateKey1);
 
   const serializedTransaction = tx.serialize();
